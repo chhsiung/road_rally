793c680fb93337ec8bcd3bdb9f592eccd4470e5f
Crystal Hsiung <crystal.hsiung@opower.com>
2011-07-18 19:00:55 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoadRally::Application.config.secret_token = '00fe4d26d252bcd2864b7dc90c1d3e55e06601d458f9f4645a1acb40fc34fbb5ce99a3bf1e3cc6d66778247db700a4090d3f53cefbef77fc90ddf4ccabdb1ad5'
