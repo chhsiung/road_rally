9ab481d269e6ad5cbf13f579e3f3ca06e283222e
chhsiung <chhsiung@gmail.com>
2011-07-19 01:02:21 -0400

    added new homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 RoadRally::Application.routes.draw do
+  resources :users
+
+  get "home/index"
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
